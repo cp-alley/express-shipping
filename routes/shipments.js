5f1399a5b7fcbb25bbcb89ddbf5be91760d4033b
Christopher Alley <calleyviola@gmail.com>
2023-09-19 19:06:37 -0400

    Add jsonschema for request body validation

--- routes/shipments.js
+++ routes/shipments.js
@@ -5,6 +5,8 @@ const { BadRequestError } = require("../expressError");
 const router = new express.Router();
 
 const { shipProduct } = require("../shipItApi");
+const jsonschema = require("jsonschema")
+const shipmentSchema = require("../schema/shipmentSchema.json")
 
 /** POST /ship
  *
@@ -18,6 +20,15 @@ router.post("/", async function (req, res, next) {
   if (req.body === undefined) {
     throw new BadRequestError();
   }
+  const result = jsonschema.validate(req.body, shipmentSchema, {required:true})
+
+  if (!result.valid) {
+    // pass validation errors to error handler
+    // (the "stack" key is generally the most useful)
+    const errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+    }
+
   const { productId, name, addr, zip } = req.body;
   const shipId = await shipProduct({ productId, name, addr, zip });
   return res.json({ shipped: shipId });
